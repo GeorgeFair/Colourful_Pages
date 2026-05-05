71ec6e47f8b276f18bf594f2c26222d0128218c0
TheBIGBOSS3826 <125705611+TheBIGBOSS3826@users.noreply.github.com>
2026-05-05 11:34:11 -0400

    programmitcally changed boxes with js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let container = document.querySelector(".container");
+
+let box1 = document.querySelector("#box1");
+box1.style.backgroundColor = "red";
+
+let box2 = document.querySelector("#box2");
+box2.style.backgroundColor = "white";
+box2.style.border = "1px solid lightgray";
+
+let box3 = document.querySelector("#box3");
+box3.style.backgroundColor = "blue";
