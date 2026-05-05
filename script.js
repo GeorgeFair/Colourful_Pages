49622115bff6e17b40e48c1f5603862a080563fa
TheBIGBOSS3826 <125705611+TheBIGBOSS3826@users.noreply.github.com>
2026-05-05 11:42:46 -0400

    added space

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ let box2 = document.querySelector("#box2");
 box2.style.backgroundColor = "white";
 box2.style.border = "1px solid black";
 
+
+
 let box3 = document.querySelector("#box3");
 box3.style.backgroundColor = "blue";
 box3.style.border = "1px solid black";
