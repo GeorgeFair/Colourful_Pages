9c275889df7a360df26e261271d9b2e422fdcdf2
TheBIGBOSS3826 <125705611+TheBIGBOSS3826@users.noreply.github.com>
2026-05-05 11:41:35 -0400

    evreything now has clearly defined black borders

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let container = document.querySelector(".container");
 container.style.backgroundColor = "lightgray";
+container.style.border = "black solid 1px";
+
 
 let box1 = document.querySelector("#box1");
 box1.style.backgroundColor = "red";
-box1.style.border = "1px solid lightgray";
+box1.style.border = "1px solid black";
 
 
 let box2 = document.querySelector("#box2");
 box2.style.backgroundColor = "white";
-box2.style.border = "1px solid lightgray";
+box2.style.border = "1px solid black";
 
 let box3 = document.querySelector("#box3");
 box3.style.backgroundColor = "blue";
-box3.style.border = "1px solid lightgray";
+box3.style.border = "1px solid black";
