af1cc47a2dcf03ea79ece845b4cd46ba63f1549c
TheBIGBOSS3826 <125705611+TheBIGBOSS3826@users.noreply.github.com>
2026-05-05 11:39:52 -0400

    added border for everything as per assignment requirement

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ container.style.backgroundColor = "lightgray";
 
 let box1 = document.querySelector("#box1");
 box1.style.backgroundColor = "red";
+box1.style.border = "1px solid lightgray";
+
 
 let box2 = document.querySelector("#box2");
 box2.style.backgroundColor = "white";
@@ -10,3 +12,4 @@ box2.style.border = "1px solid lightgray";
 
 let box3 = document.querySelector("#box3");
 box3.style.backgroundColor = "blue";
+box3.style.border = "1px solid lightgray";
